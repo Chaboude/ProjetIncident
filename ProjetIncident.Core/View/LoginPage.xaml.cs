89fe07ceb442b33fb1e251b48240dbff39d9d718
Thomas <thomas.chaboud@etu.univ-lyon1.fr>
2018-04-06 14:37:06 +0200

    FEAT Ajout projet Xamarin

---
+++ ProjetIncident.Core/View/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+namespace ProjetIncident.Core.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+		public void OnButtonClicked(object sender, EventArgs args)
+		{
+			Button button = (Button)sender;
+            App.Current.MainPage = NavigationService.GetCurrent().RootPage;
+		}
+    }
+}
---
+++ ProjetIncident.Core/View/MenuPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ProjetIncident.Core.View
+{
+	public partial class MenuPage : ContentPage
+	{
+		public MenuPage()
+		{
+			Title = "Menu";
+			InitializeComponent();
+			// Add your UI code for the menu here or in your XAML file
+		}
+	}
+}
